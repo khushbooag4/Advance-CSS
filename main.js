5a7aaa363dd2cfe0c823f82b04a758279ffd49fc
Khushboo <kushbooagarwal3584@gmail.com>
2021-11-03 17:39:24 +0530

    Slider CSS added

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let wrapper = document.getElementById('wrapper');
+    let handle = wrapper.querySelector('.handle');
+    let topLayer = wrapper.querySelector('.top');
+    let skew =0;
+    let delta = 0;
+
+    if(wrapper.className.indexOf('skewed') != -1){
+        skew =  1000;
+    }
+    wrapper.addEventListener('mousemove', function(e){
+        //console.log(e.clientX);
+        delta = (e.clientX - window.innerWidth / 2)*0.5;
+        handle.style.left = e.clientX + delta + 'px';
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    });
+});
